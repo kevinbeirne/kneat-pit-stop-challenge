ad83d50dfc86d715ef8cdc9928148eb628e271ac
kevinbeirne <kevin@conjure.ie>
2018-07-03 12:57:26 +0100

    Completed validation and documentation

--- js/main.js
+++ js/main.js
@@ -1,54 +1,70 @@
 
 
 /**
-*@class Entry point for the app
+* @class Entry point for the app
+* This class controls the DOM for our pitstop calculator app
+* It will set up an interface that allows a user to enter a number of Megalights and will display
+* all the starships and the total amount of stops required to make the given distance.
+* It will give feedback to the user about whether their request was successful
 */
 function Main() {
   this.loading = false;
 
+  /**
+  * Errors
+  */
   this.DISTANCE_ERROR = "Please enter a valid distance";
   this.LOAD_FAILED_ERROR = "Couldn't fetch all the results, please try again later";
-
 
   /**
   * Dom values
   */
   this.CALCULATE_BUTTON = "mglt-calculate-btn";
   this.MGLT_INPUT = "mglt-input";
-  this.IGNORE_UNKNOWNS_CHECK = "ignore-unknowns-chk";
+  this.IGNORE_UNKNOWN_PITSTOPS_CHECK = "ignore-unknowns-chk";
   this.STARSHIP_LIST = "starship-list";
   this.LOADER = "loader";
   this.ERROR_MESSAGE = "error-message";
 }
 
-Main.prototype.getCalculateButton = function() {
-
-};
-
+/**
+* Initialises the app and enables user controls to calculate starship pitstops
+*/
 Main.prototype.init = function() {
   this.starshipManager = new StarshipManager();
   var calculateBtn = document.getElementById(this.CALCULATE_BUTTON);
-  var self = this;
 
   if(calculateBtn) {
-    calculateBtn.addEventListener("click", function(evt) {
-        if(self.getDistance() > 0) {
-          self.toggleLoading();
-          self.setErrorMessage("");
-          self.calculateStarshipPitstops();
-        } else {
-          self.setErrorMessage(self.DISTANCE_ERROR);
-        }
-      }, false);
+    calculateBtn.addEventListener("click", this.onCalculatePitstops.bind(this), false);
   }
 };
 
+/**
+* Make a request to calculate the number of pitstops
+*/
+Main.prototype.onCalculatePitstops = function() {
+  var distance = this.getDistance();
+  if(distance > 0 && distance <= this.getMaxDistance()) {
+    this.toggleLoading();
+    this.setErrorMessage("");
+    this.calculateStarshipPitstops();
+  } else {
+    this.setErrorMessage(this.DISTANCE_ERROR);
+  }
+};
 
+/**
+* Empties our list of current starships and begins an attempt
+* to recalculate all starship pitstops
+*/
 Main.prototype.calculateStarshipPitstops = function() {
-  this.clearSpaceships();
+  this.clearStarships();
   this.starshipManager.loadMoreStarships().then(this.getMoreStarships.bind(this), this.loadFailed.bind(this));
 };
 
+/**
+* This will update the pitstops of our
+*/
 Main.prototype.getMoreStarships = function(results) {
   this.updatePitstops(results);
   var promise = this.starshipManager.loadMoreStarships();
@@ -59,38 +75,41 @@ Main.prototype.getMoreStarships = function(results) {
   }
 };
 
+/**
+* Clear any partially loaded data and display an error if we fail to load starsgips
+*/
 Main.prototype.loadFailed = function() {
-  this.clearSpaceships();
+  this.clearStarships();
   this.setErrorMessage(this.LOAD_FAILED_ERROR);
 };
 
-Main.prototype.updatePitstops = function(res) {
+
+/**
+* Goes through each of the given starships and displays them
+* @param {array} starships An array of the starships we want to update the pitstops of. Should each have name, MGLT and consumables properties
+*/
+Main.prototype.updatePitstops = function(starships) {
   var distance = this.getDistance();
 
+  for(var i = 0; i < starships.length; i++) {
+    var consumablesInHours = this.starshipManager.getConsumablesInHours(starships[i]);
 
-  for(var i = 0; i < res.length; i++) {
-    var AMOUNT = 0;
-    var UNIT = 1;
-    var consumables = res[i].consumables.split(" ");
-    var consumablesInHours = -1;//Fix this
-    if(consumables.length == 2){
-      consumablesInHours = this.starshipManager.getConsumablesInHours(consumables[AMOUNT], consumables[UNIT]);
-    }
-
-    if(distance < 0 || consumablesInHours < 0 || res[i].MGLT == "unknown" || res[i].MGLT < 0) {
-      var unknowns = document.getElementById(this.IGNORE_UNKNOWNS_CHECK);
-      if(unknowns && !unknowns.checked) {
-        this.displaySpaceship(res[i].name, "unknown");
+    if(distance < 0 || consumablesInHours < 0 || starships[i].MGLT == "unknown" || starships[i].MGLT < 0) {
+      if(this.allowUnknownPitstops()) {
+        this.displayStarship(starships[i].name, "unknown");
       }
     } else {
-      var stopsRequired = Math.floor(distance / (res[i].MGLT * consumablesInHours));
-      this.displaySpaceship(res[i].name, stopsRequired);
+      var stopsRequired = Math.floor(distance / (starships[i].MGLT * consumablesInHours));
+      this.displayStarship(starships[i].name, stopsRequired);
     }
   }
 
 };
 
-Main.prototype.clearSpaceships = function() {
+/**
+* Clears the display of all starships
+*/
+Main.prototype.clearStarships = function() {
   var starshipList = document.getElementById(this.STARSHIP_LIST);
   if(starshipList) {
     starshipList.innerHTML = "";
@@ -98,11 +117,11 @@ Main.prototype.clearSpaceships = function() {
 };
 
 /**
+* Displays the given sparship with the given detail by adding it to a list
 * @param {string} name The name of the ship we want to display
 * @param {string} detail The detail about this ship we want to display
 */
-Main.prototype.displaySpaceship = function(name, detail) {
-  console.log(name + ": " + detail);
+Main.prototype.displayStarship = function(name, detail) {
   var starshipList = document.getElementById(this.STARSHIP_LIST);
   if(starshipList) {
     var li = document.createElement("li");
@@ -115,8 +134,8 @@ Main.prototype.displaySpaceship = function(name, detail) {
 
 
 /**
-* Updates the dom to the loading state
-*
+* Toggles whether app is loading or not
+* @return {boolean} Whether the app is loading after the toggle
 */
 Main.prototype.toggleLoading = function() {
   this.loading = !this.loading;
@@ -137,8 +156,12 @@ Main.prototype.toggleLoading = function() {
   if(input) {
     input.disabled = this.loading;
   }
+  return this.loading;
 };
 
+/**
+* Sends the given error message to the user
+*/
 Main.prototype.setErrorMessage = function(message) {
   var messageBox = document.getElementById("error-message");
   if(messageBox) {
@@ -146,12 +169,37 @@ Main.prototype.setErrorMessage = function(message) {
   }
 };
 
+/**
+* Get the distance the user has provided us with
+* @return {number} The distance the user has entered or -1 if the user input was invalid or undetectable
+*/
 Main.prototype.getDistance = function() {
   var input = document.getElementById("mglt-input");
   if(input && input.value && !isNaN(input.value)) {
-    return input.value;
+    return parseInt(input.value);
   }
   return -1;
+};
+
+/**
+* Gets the max distance a user is allowed to enter
+* @return {number} The max distance a user can enter. -1 If not found.
+*/
+Main.prototype.getMaxDistance = function() {
+  var input = document.getElementById("mglt-input");
+  if(input && input.max && !isNaN(input.max)) {
+    return parseInt(input.max);
+  }
+  return -1;
+};
+
+/**
+* Returns whether we are allowing or disallowing unknown pitstops to be displayed
+* @return {boolean} True if unknowns are allowed, false otherwise
+*/
+Main.prototype.allowUnknownPitstops = function() {
+  var excludeUnknowns = document.getElementById(this.IGNORE_UNKNOWN_PITSTOPS_CHECK);
+  return(excludeUnknowns && !excludeUnknowns.checked);
 };
 
 /**
--- js/starshipManager.js
+++ js/starshipManager.js
@@ -1,42 +1,16 @@
+/**
+* @class
+* This StarshipManager will allow a user to request starships and perform actions on a given starship
+* e.g. Get a given starship's consumables available in hours
+*/
 StarshipManager = function(){
   this.nextRequest = "https://swapi.co/api/starships/";
-  this.API_UNKOWN = "unknown";//The value the api uses to tell us it doesn't have an answer
-};
-/**
-* Gets all the starships from thje swapi
-* @param {number} count The max number of starships we want to use
-* @return {Object[]} The list of starships
-*/
-StarshipManager.prototype.loadAllStarships = function(count) {
-  return new Promise(function(resolve, reject) {
-    var xhttp = new XMLHttpRequest();
-    var nextRequest = "https://swapi.co/api/starships/";
-    var results = [];
-    var err = null;
-    while(nextRequest) {
-      xhttp.open("GET", nextRequest, false);
-      xhttp.setRequestHeader("Content-type", "application/json");
-      xhttp.send();
-      var response = JSON.parse(xhttp.responseText);
-      if(xhttp.status == 200) {
-        if(response.results && response.results.constructor === Array) {
-          results.push(...response.results);
-        } else {
-          err = "No results were in the response";
-        }
-        nextRequest = response.next;
-      } else {
-        nextRequest = null;
-        err = xhttp.status;
-      }
-      if(err) {
-        return reject(err);
-      }
-    }
-    resolve(results);
-  });
 };
 
+/**
+* This will load a batch of starships from the swapi.
+* @return {Promise} A promise that will resolve when the starships are loaded. Is null if all starships have been loaded. Another request to this service after it returns null will begin loading the starships from the beginning
+*/
 StarshipManager.prototype.loadMoreStarships = function() {
   if(!this.nextRequest) {//Reached the end of our requests, let user know by returning null
     this.nextRequest = "https://swapi.co/api/starships/";
@@ -78,30 +52,48 @@ StarshipManager.prototype.loadMoreStarships = function() {
 };
 
 /**
-* Reads the given amount and unit and returns how long the consumables will last, in hours
+* Gets the amount of consumables the given starship has available, in hours
+* @param {Object} starship An object which is expected to have the property "consumables" which should take the format "number unit" e.g. "5 days", "7 years" etc.
+* @return {number} The amount of consumables the given starship has, measured in hours
 */
-StarshipManager.prototype.getConsumablesInHours = function(amount, unit) {
+StarshipManager.prototype.getConsumablesInHours = function(starship) {
+  var AMOUNT = 0;
+  var UNIT = 1;
+  var consumables = starship.consumables.split(" ");
+  var consumablesInHours = -1;
+  if(consumables.length != 2) {
+    return consumablesInHours;
+  }
+
+  return this._convertToHours(consumables[AMOUNT], consumables[UNIT]);
+};
+
+/**
+* Takes the given amount and converts it to hours
+* @param {number} amount The amount of the given unit we want converted into hours
+* @param {string} unit The unit of measurement the amount is currently in. Valid values: Any that start with "day","week","month","year"
+* @return {number} The amount provided, converted to hours
+*/
+StarshipManager.prototype._convertToHours = function(amount, unit) {
   var HOURS_PER_YEAR = 8766;
   var DAYS_PER_YEAR = 365;
   var WEEKS_PER_YEAR = 52;
   var MONTHS_PER_YEAR = 12;
-
-  var consumablesInHours = -1;
-
+  var amountInHours = -1;
   if(!isNaN(amount) && typeof(unit == "string")) {
-    consumablesInHours = amount;
+    amountInHours = amount;
 
     if(unit.startsWith("day")) {
-      consumablesInHours *= HOURS_PER_YEAR / DAYS_PER_YEAR;
+      amountInHours *= HOURS_PER_YEAR / DAYS_PER_YEAR;
     } else if(unit.startsWith("week")) {
-      consumablesInHours *= HOURS_PER_YEAR / WEEKS_PER_YEAR;
+      amountInHours *= HOURS_PER_YEAR / WEEKS_PER_YEAR;
     } else if(unit.startsWith("month")) {
-      consumablesInHours *= HOURS_PER_YEAR / MONTHS_PER_YEAR;
+      amountInHours *= HOURS_PER_YEAR / MONTHS_PER_YEAR;
     } else if (unit.startsWith("year")) {
-      consumablesInHours *= HOURS_PER_YEAR;
+      amountInHours *= HOURS_PER_YEAR;
     } else {
-      consumablesInHours = -1;
+      amountInHours = -1;
     }
   }
-  return consumablesInHours;
+  return amountInHours;
 };
